05c3dca7bf44baab407190ed6bd097dfccd65f82
nandanahari04 <93121007+nandanahari04@users.noreply.github.com>
2022-04-30 09:24:32 +0530

    Add files via upload

---
+++ jdoodle.c
@@ -0,0 +1,67 @@
+//c program to implement employee details using structures
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct{
+    char name[30];
+    int id;
+    int experience;
+    double salary;
+} Employee;
+ 
+int main()
+{
+    //number of employees
+    int n=5;
+
+    //array to store structure values of all employees
+    Employee employees[n];
+ 
+    //Taking each employee detail as input
+    printf("Enter %d Employee Details \n \n",n);
+    for(int i=0; i<n; i++){
+        printf("Employee %d:- \n",i+1);
+
+        //Name
+        printf("Name: ");
+        scanf("%[^\n]s",employees[i].name);
+
+        //ID
+        printf("Id: ");
+        scanf("%d \n" ,&employees[i].id);
+        
+        //Experience
+        printf("Experience: ");
+        scanf("%d\n",&employees[i].experience);
+
+        //Salary
+        printf("Salary: ");
+        scanf("%lf \n",&employees[i].salary);
+
+        //to consume extra '\n' input
+        char ch = getchar();
+ 
+        printf("\n");
+    }
+ 
+    //Displaying Employee details
+    printf("-------------- All Employees Details ---------------\n");
+    for(int i=0; i<n; i++){
+        printf("Name \t: ");
+        printf("%s \n",employees[i].name);
+ 
+        printf("Id \t: ");
+        printf("%d \n",employees[i].id);
+        
+        printf("Experience \t: ");
+        printf("%d \n",employees[i].experience);
+ 
+ 
+        printf("Salary \t: ");
+        printf("%.2lf \n",employees[i].salary);
+ 
+        printf("\n");
+    }
+ 
+    return 0;
+}
